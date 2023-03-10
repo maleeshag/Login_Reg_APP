f4957310326078d19afcbd1dadd2773c956650c6
maleeshagimmana <it21176456@my.sliit.lk>
2023-03-10 21:13:19 +0530

    register done

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,7 +1,74 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {Link} from 'react-router-dom'
+import avatar from '../assets/profile.png'
+import styles from '../styles/Username.module.css'
+import {Toaster} from 'react-hot-toast';
+import {useFormik} from 'formik';
+import {passwordValidate} from '../helper/validate'
+import convertToBase64 from '../helper/convert'
 
-export default function register(){
+export default function Register(){
+
+    const [file,setFile]=useState();
+
+    const formik = useFormik({
+        initialValues:{
+            email:'',
+            username:'',
+            password:''
+        },
+        validate:passwordValidate,
+        validateONBlur:false,
+        validateOnChange:false,
+        onSubmit:async values=>{
+            values = await Object.assign(values,{profile:file || ''})
+             console.log(values)
+        }
+
+    })
+
+/**Formik does't support file upload so we need to create this handler */
+const onUpload=async e=>{
+    const base64=await convertToBase64(e.target.files[0]);
+    setFile(base64);
+}
+
     return(
-        <div>Register</div>
+        <div className='container mx-auto'>
+            
+            <Toaster position='top-center' reverseOrder={false}></Toaster> 
+
+            <div className='flex justify-center items-center h-screen'>
+                <div className={styles.glass} style={{width:"45%"}}>
+                    <div className='title flex flex-col items-center'>
+                        <h4 className='text-4xl font-bold'>Register</h4>
+                        <span className='py-4 text-xl w-2/3 text-center'>
+                            Happy to join you!
+                        </span>
+                    </div>
+                    <form className='py-1' onSubmit={formik.handleSubmit}>
+                        <div className='profile flex justify-center py-4'>
+                            <label htmlFor='profile'>
+                            <img src={file || avatar} className={styles.profile_img} alt="avatar"></img>
+                            </label>
+                            <input onChange={onUpload} type="file" id="profile" name="profile"></input>
+
+                            
+                           
+                        </div>
+                        <div className='textbox flex flex-col items-center gap-6'>
+                            <input {...formik.getFieldProps('email')} type="email" className={styles.textbox} placeholder='Email*'></input>
+                            <input {...formik.getFieldProps('username')} type="text" className={styles.textbox} placeholder='Username'></input>
+                            <input {...formik.getFieldProps('password')} type="password" className={styles.textbox} placeholder='password'></input>
+                            <button type='submit' className={styles.btn}>Register</button>
+                        </div>
+                        <div className='text-center py-4'>
+                            <span>Already Register?<Link className='text-red-500' to='/'>Login Now</Link></span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
     )
 }
