34c2f94d3ec2dc48d49a8fd46363000762815cb3
maleeshagimmana <it21176456@my.sliit.lk>
2023-03-10 18:02:41 +0530

    login ui

--- client/src/components/Username.js
+++ client/src/components/Username.js
@@ -1,7 +1,56 @@
 import React from 'react';
+import {Link} from 'react-router-dom'
+import avatar from '../assets/profile.png'
+import styles from '../styles/Username.module.css'
+import {Toaster} from 'react-hot-toast';
+import {useFormik} from 'formik';
+import {usernameValidate} from '../helper/validate'
 
 export default function Username(){
+
+    const formik = useFormik({
+        initialValues:{
+            username:''
+        },
+        validate:usernameValidate,
+        validateONBlur:false,
+        validateOnChange:false,
+        onSubmit:async values=>{
+             console.log(values)
+        }
+
+    })
+
+
+
     return(
-        <div>User Name</div>
+        <div className='container mx-auto'>
+            
+            <Toaster position='top-center' reverseOrder={false}></Toaster> 
+
+            <div className='flex justify-center items-center h-screen'>
+                <div className={styles.glass}>
+                    <div className='title flex flex-col items-center'>
+                        <h4 className='text-4xl font-bold'>Register Now</h4>
+                        <span className='py-4 text-xl w-2/3 text-center'>
+                            Explore More by connecting with us
+                        </span>
+                    </div>
+                    <form className='py-1' onSubmit={formik.handleSubmit}>
+                        <div className='profile flex justify-center py-4'>
+                            <img src={avatar} className={styles.profile_img} alt="avatar"></img>
+                        </div>
+                        <div className='textbox flex flex-col items-center gap-6'>
+                            <input {...formik.getFieldProps('username')} type="text" className={styles.textbox} placeholder='Username'></input>
+                            <button type='submit' className={styles.btn}>Let's Go</button>
+                        </div>
+                        <div className='text-center py-4'>
+                            <span>Not a Member<Link className='text-red-500' to='/register'> Register</Link></span>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
     )
 }
