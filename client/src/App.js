3b3df634ddbecb34707b315ed90a67af2a00eb77
maleeshagimmana <it21176456@my.sliit.lk>
2023-03-10 09:57:47 +0530

    created components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {createBrowserRouter,RouterProvider} from 'react-router-dom';
+
+/**import all components */
+import Username from './components/Username'
+import PageNotFound from './components/PageNotFound'
+import Profile from './components/Profile'
+import Password from './components/Password'
+import Recovery from './components/Recovery'
+import Register from './components/Register'
+import Reset from './components/Reset'
+
+
+
+/*root routers */
+/*this create different components on differnet routes */
+const router =createBrowserRouter([
+  {
+    path:'/',
+    element:<Username></Username>
+  },
+  {
+    path:'/register',
+    element:<Register></Register>
+  },
+  {
+    path:'/password',
+    element:<Password></Password>
+  },
+  {
+    path:'/profile',
+    element:<Profile></Profile>
+  },
+  {
+    path:'*',
+    element:<PageNotFound></PageNotFound>
+  },
+  {
+    path:'/recovery',
+    element:<Recovery></Recovery>
+  },
+  {
+    path:'/reset',
+    element:<Reset></Reset>
+  },
+])
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>  
+    </main>
   );
 }
 
---
+++ client/src/components/Recovery.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Password(){
+    return(
+        <div>User Name</div>
+    )
+}
